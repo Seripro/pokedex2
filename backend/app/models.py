975188a0c57d4dfeb93261dba658f7782041986a
keigo <2364138keigo@gmail.com>
2026-05-20 08:40:14 +0900

    favoritesのカラムにmemoを追加

--- backend/app/models.py
+++ backend/app/models.py
@@ -8,6 +8,7 @@ class Favorite(Base):
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(String, index=True, nullable=False)
     pokemon_id = Column(Integer, index=True, nullable=False)
+    memo = Column(String, index=True, nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
 
     # 💡 実務テクニック: 同じユーザーが同じポケモンを2回重複して登録できないようにするユニーク制約
