d0912c12130e5f7468b4b5604343a9a48eefbdda
keigo <2364138keigo@gmail.com>
2026-05-19 17:15:00 +0900

    レイアウトを配置

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,18 @@ import { Home } from "./pages/Home";
 import { PokemonDetail } from "./pages/PokemonDetail";
 import Demo from "./pages/Demo";
 import Favorites from "./pages/Favorites";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/detail/:id" element={<PokemonDetail />} />
-        <Route path="/demo" element={<Demo />} />
-        <Route path="/favorites" element={<Favorites />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/detail/:id" element={<PokemonDetail />} />
+          <Route path="/demo" element={<Demo />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Route>
       </Routes>
     </Router>
   );
