5325e5769eb59c32fb72bdb2e9b9ef80961bc2d5
keigo <2364138keigo@gmail.com>
2026-05-19 15:01:55 +0900

    Demoのルーティング設定

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { PokemonDetail } from "./pages/PokemonDetail";
+import Demo from "./pages/Demo";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/detail/:id" element={<PokemonDetail />} />
+        <Route path="/demo" element={<Demo />} />
       </Routes>
     </Router>
   );
