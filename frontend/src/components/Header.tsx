cf9ad9612dc7928553adc71f52e10125ae54a20c
keigo <2364138keigo@gmail.com>
2026-05-20 19:55:48 +0900

    ログアウトボタンを追加し、押下時にuseAuthのログアウト関数を実行

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,32 @@
 import { Link } from "react-router-dom";
+import { useAuth } from "../context/useAuth";
 
 function Header() {
+  const { session, signOut } = useAuth();
+
   return (
-    <div>
-      <Link to="/">ホーム</Link>
-      <Link to="/favorites">お気に入り</Link>
-    </div>
+    <header className="site-header">
+      <div className="site-header__brand">
+        <Link to="/" className="site-header__title">
+          Pokédex
+        </Link>
+      </div>
+      <nav className="site-header__nav">
+        <Link to="/">ホーム</Link>
+        <Link to="/favorites">お気に入り</Link>
+        {session ? (
+          <button
+            type="button"
+            className="site-header__button"
+            onClick={signOut}
+          >
+            ログアウト
+          </button>
+        ) : (
+          <Link to="/login">ログイン</Link>
+        )}
+      </nav>
+    </header>
   );
 }
 
